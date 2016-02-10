7a8cd0322a518149d17f44051cb48c67ad7c889d
Michael_Jazgarski <mj1488@nyu.edu>
2016-02-09 22:43:08 -0500

    commiting 	modified:   strlen.c 	#strlen.c# 	Temperature 	a.out 	temperature.c yes

--- strlen.c
+++ strlen.c
@@ -1,9 +1,17 @@
-int stringlength(char string[])
-{
-	
-}
+#include <stdio.h>
+
+int stringlength(char string[]);
 
 int main()
 {
+	char str[] = "The quick brown fox";
+
+	printf ("The length of the string is %s is %d\n ", str, stringlength(str));
+
+}
+
+int stringlength(char string[])
+{
+
 
 }
