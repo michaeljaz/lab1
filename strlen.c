cbdb441a32d40f98cabc19002a7a22f0bb3036ee
Michael_Jazgarski <mj1488@nyu.edu>
2016-02-10 12:12:29 -0500

    new commit

--- strlen.c
+++ strlen.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
-
-int stringlength(char string[]);
+#include <string.h>
 
 int main()
 {
 	char str[] = "The quick brown fox";
+	int length;
 
-	printf ("The length of the string is %s is %d\n ", str, stringlength(str));
+	length = strlen(str);
+
+	printf ("The length of the string is %s is %d\n ", str, length);
+
+	return(0);
 
 }
 
-int stringlength(char string[])
-{
-
-
-}
