75f1fc02d80c3cd053d6f6befe6ff70aa451fba4
Michael_Jazgarski <mj1488@nyu.edu>
2016-02-10 12:39:51 -0500

    new commit

--- temperature.c
+++ temperature.c
@@ -2,16 +2,22 @@
 
 int main()
 {
-	int i;
-	int Celsius;
+	float farenheit, celsius;
+	int low, high, step;
 
-	for(i = 0; i <= 300; i++)
+	low = 0;
+	high = 300;
+	step = 10;
+	printf(" F      C");
+	printf("\n--------------");
+
+	farenheit = low;
+	while (celsius <= high)
 	{
-	Celsius = ((5/9) * (i-32));
-
-	printf("The temperature in Celsius is %d\n", Celsius);
+		celsius = (9 / 5) * farenheit - 32;
+		printf("\n%3.1f %3.1f\n", farenheit, celsius);
+		farenheit = farenheit + step;
 	}
-	return 0;
 
-
+	return(0);
 }
